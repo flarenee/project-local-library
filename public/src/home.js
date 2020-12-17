f28945cabc10964dda084069612569d14306f634
Jacob Brown <flarenee13@gmail.com>
2020-12-17 08:55:30 -0500

    message

--- public/src/home.js
+++ public/src/home.js
@@ -17,7 +17,7 @@ function totalBooksCount(books) {
    return total;
  };
  
- function getMostCommonGenres(books) {
+ function MostCommonGenres(books) {
    const booksGenre = [];
    for (let key in books) { booksGenre.push(books[key].genre) };
    const uniqueGenre = [...new Set(booksGenre)];
@@ -32,7 +32,7 @@ function totalBooksCount(books) {
    return bookGenreTotals.slice(0, 5);
  };
  
- function getMostPopularBooks(books) {
+ function MostPopularBooks(books) {
    const popularBookTotals = [];
    for (let key in books) {
      popularBookTotals.push({name: books[key].title, count: books[key].borrows.length});
@@ -43,7 +43,7 @@ function totalBooksCount(books) {
    return popularBookTotals.slice(0, 5);
  };
  
- function getMostPopularAuthors(books, authors) {
+ function MostPopularAuthors(books, authors) {
      const popularAuthorsTotals = [];
  
    for (let key in authors) {
